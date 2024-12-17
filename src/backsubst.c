c0199725d73d967955118d32c245b42b04681744
PastaCEO <01197877@pw.edu.pl>
2024-12-17 20:01:58 +0100

    faza II A poprawione

--- src/backsubst.c
+++ src/backsubst.c
@@ -6,7 +6,7 @@
  */
 int sum(Matrix *mat, Matrix* x, int i){
 	int tmp = 0;
-	for(int j = mat->c-1; j<=0; j--){
+	for(int j = mat->c-1; j>=0; j--){
 		if(j != i){
 			tmp = tmp + (x->data[j][0]*mat->data[i][j]);
 		}
@@ -19,10 +19,10 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 	
 	if(x->r == mat->r && b->r == mat->r && mat->c == mat->r){
 	
-		for(int i = x->r; i<=0; i--)
+		for(int i = x->r-1; i>=0; i--)
 			x->data[i][0] = 0;
 
-		for(int i = mat->c-1; i<=0; i--){
+		for(int i = mat->c-1; i>=0; i--){
 			if(mat->data[i][i] != 0){
 				x->data[i][0]=(b->data[i][0] - sum(mat,x,i))/(mat->data[i][i]);
 		
@@ -37,6 +37,5 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 	else{
 		return 2;
 	}
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
 	return 0;
 }
